c458132753f2ef533b46c90ef567c2009b8e638d
Darius Serdean <darius23cj@yahoo.com>
2017-02-02 21:22:27 +0200

    change password in 3 moduri

--- src/main/java/org/fasttrackit/automation/ChangePasswordPage.java
+++ src/main/java/org/fasttrackit/automation/ChangePasswordPage.java
@@ -3,6 +3,9 @@ package org.fasttrackit.automation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import java.sql.Driver;
 
 
 public class ChangePasswordPage {
@@ -15,11 +18,34 @@ public class ChangePasswordPage {
         @FindBy(name = "newPasswordRepeat")
         private WebElement repeatPasswordField;
 
+        @FindBy(xpath = "//*[@id='preferences-win']//button[normalize-space(text())='Save']")
+        private WebElement saveBtn;
+
+        @FindBy(className = "status-msg")
+        private  WebElement statusMsg;
+
+
+        public void changePassword(String currentPassword,String newPass){
+               changePassword(currentPassword,newPass,newPass);
+
+        }
+
+
+
+
+
 
         public void changePassword(String currentPassword,String newPass, String newPassRepeat){
                 currentPasswordField.sendKeys(currentPassword);
                 newPasswordField.sendKeys(newPass);
                 repeatPasswordField.sendKeys(newPassRepeat);
+                saveBtn.click();
+
         }
 
+        public String getStatusMessage() {
+                String msg =statusMsg.getText();
+                System.out.println(msg);
+                return msg;
+        }
 }
--- src/test/java/org/fasttrackit/automation/ChangePasswordTest.java
+++ src/test/java/org/fasttrackit/automation/ChangePasswordTest.java
@@ -5,11 +5,16 @@ import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.internal.Utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class ChangePasswordTest extends TestBase {
     private LoginPage loginPage;
-    private  ChangePasswordPage page;
+    private ChangePasswordPage page;
 
     public ChangePasswordTest() {
         loginPage = PageFactory.initElements(driver, LoginPage.class);
@@ -19,6 +24,34 @@ public class ChangePasswordTest extends TestBase {
 
     @Test
     public void changePasswordWithInvalidCurrentPassword() {
+        openPage();
+        page.changePassword("wrong.pass", "new.pass", "new.pass");
+
+        assertThat(page.getStatusMessage(), is("Your preview password is incorrect!"));
+
+
+    }
+
+    @Test
+    public void changePasswordWithInvalidRepeatPassword() {
+        openPage();
+        page.changePassword("eu.pass", "new1.pass", "new.pass");
+
+        assertThat(page.getStatusMessage(), is("Password does not match the confirm password!"));
+
+    }
+
+    @Test
+    public void successfullyChangePassword() {
+        openPage();
+        page.changePassword("eu.pass", "new1.pass");
+
+        assertThat(page.getStatusMessage(), is("Your password has been successfully changed."));
+
+
+    }
+
+    private void openPage() {
         openBrowser();
 
         loginPage.login("eu@fast.com", "eu.pass");
@@ -26,23 +59,7 @@ public class ChangePasswordTest extends TestBase {
         WebElement preferencesBtn = driver.findElement(By.xpath("//button[@data-target='#preferences-win']"));
         preferencesBtn.click();
 
-        changePasswordFields("eu.pass", "new.pass", "new.pass");
-
-
+        com.sdl.selenium.web.utils.Utils.sleep(1000);
     }
 
-
-    private void changePasswordFields(String currentPassword, String newPassword, String confirmPassword) {
-
-
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        page.changePassword("wrong.pass", "new.pass", "new.pass");
-
-    }
 }
